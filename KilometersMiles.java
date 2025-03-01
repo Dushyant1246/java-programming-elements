ba3f9ed327a966973f182ad6678335f93f040f5f
Dushyant1246 <dushyant.dhmn808@gmail.com>
2025-03-01 09:19:28 +0530

    [Dushyant] ADD: Convert kilometers to miles

---
+++ KilometersMiles.java
@@ -0,0 +1,15 @@
+// KilometersMiles class to convert kilometers into miles
+
+class KilometersMiles{
+	public static void main(String[] args){
+		
+		// Variable to indicate kilometers value
+		float kilometers = 10.8f;
+		
+		// Variable to indicate miles value
+		float miles = kilometers / 1.6f;
+		
+		// Print value in miles
+		System.out.println("The distance " + kilometers + " km in miles is " + miles);
+	}
+}
